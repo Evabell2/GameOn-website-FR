6e573a6a2fbeebd4e21bd2ad058e64da23bec860
Bellouard <lyn-n@live.fr>
2021-12-10 12:41:24 +0100

    + fct validation formulaire [fix] event de validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,52 +9,61 @@ function editNav() {
 }
 
 //#region TODO : fermer la modale
-  // DOM Elements
-  const modalbg = document.querySelector(".bground");
-  const modalBtn = document.querySelectorAll(".modal-btn");
-  const closeModalBtn = document.querySelectorAll(".close");
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelectorAll(".close");
 
-  // launch modal event
-  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-  // close modal event
-  closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
 
-  // launch modal form
-  function launchModal() {
-    modalbg.style.display = "block";
-  }
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
 
-  // close modal form
-  function closeModal() {
-    modalbg.style.display = "none";
-  }
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 //#endregion TODO : fermer la modale
 
 
 // const formData = document.querySelectorAll(".formData");
 // const form = document.getElementById ("form");
-  
-// #region message de validation
-  // const messageValidation = document.getElementById("message_validation");
-  // const isvalid = validate();
 
-  // document.getElementById("envoyer").addEventListener('click', function() {
-  //   // if (isvalid === true) {
-  //     messageValidation.innerHTML = "Merci ! Votre réservation a été reçue."
-  //   // }
-  // })
+// #region message de validation
+// const messageValidation = document.getElementById("message_validation");
+// const isvalid = validate();
+
+// document.getElementById("envoyer").addEventListener('click', function() {
+//   // if (isvalid === true) {
+//     messageValidation.innerHTML = "Merci ! Votre réservation a été reçue."
+//   // }
+// })
 // #endregion message de validation
 
 
-document.getElementById("envoyer").addEventListener('click', function validate () {
-  
+document.getElementById("envoyer").addEventListener('click', (event) => {
+
+  event.preventDefault();
+
+  if (true || validateForm()) {
+    document.getElementById("modalBody").innerHTML = `test <br><br><p class="">ddd</p>`;
+  }
+})
+
+function validateForm() {
+  let isValidForm = true
   // PRENOM
   const prenom = document.getElementById("first");
   const prenom_m = document.getElementById("prenom_erreur");
 
-  if (prenom.value === '' || prenom.value.length< 2) { 
+  if (prenom.value === '' || prenom.value.length < 2) {
     prenom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-    e.preventDefault();
+    isValidForm = false;
   }
   else {
     prenom_m.textContent = "";
@@ -63,34 +73,34 @@ document.getElementById("envoyer").addEventListener('click', function validate (
   const nom = document.getElementById("last");
   const nom_m = document.getElementById("nom_erreur");
 
-  if (nom.value === '' || nom.value.length< 2) { 
+  if (nom.value === '' || nom.value.length < 2) {
     nom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-    e.preventDefault();
+    isValidForm = false;
   }
   else {
     nom_m.textContent = "";
   }
 
   // MAIL
-  const email =  document.getElementById("email");
+  const email = document.getElementById("email");
   const email_m = document.getElementById("email_erreur");
   var mailCaractere = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
 
-  if(mailCaractere.test(email.value)){
+  if (mailCaractere.test(email.value)) {
     email_m.textContent = "";
   }
   else {
-    email_m.textContent =" Veuillez entrer un email correct";
-    e.preventDefault();
+    email_m.textContent = " Veuillez entrer un email correct";
+    isValidForm = false;
   }
 
   // NAISSANCE
   const naissance = document.getElementById("birthdate");
   const naissance_m = document.getElementById("birthdate_erreur");
 
-  if (naissance.value ==='') {
-    naissance_m.textContent= "Veuillez entrez votre date de naissance";
-    e.preventDefault();
+  if (naissance.value === '') {
+    naissance_m.textContent = "Veuillez entrez votre date de naissance";
+    isValidForm = false;
   }
   else {
     naissance_m.textContent = "";
@@ -102,7 +112,7 @@ document.getElementById("envoyer").addEventListener('click', function validate (
 
   if (nombreTournois.value === '') {
     nombreTournois_m.textContent = "Veuillez entrer le nombre de tournois participé";
-    e.preventDefault();
+    isValidForm = false;
   }
   else {
     nombreTournois_m.textContent = "";
@@ -110,30 +120,33 @@ document.getElementById("envoyer").addEventListener('click', function validate (
 
   // SELECTION VILLE
   const location1 = document.getElementById("location1");
-  const location2 =  document.getElementById("location2");
-  const location3 =  document.getElementById("location3");
-  const location4 =  document.getElementById("location4");
-  const location5 =  document.getElementById("location5");
-  const location6 =  document.getElementById("location6");
+  const location2 = document.getElementById("location2");
+  const location3 = document.getElementById("location3");
+  const location4 = document.getElementById("location4");
+  const location5 = document.getElementById("location5");
+  const location6 = document.getElementById("location6");
   const location_m = document.getElementById("location_manquant");
 
-  if ((location1.checked)|| (location2.checked) || (location3.checked) ||(location4.checked) ||(location5.checked) ||(location6.checked)) {
+  if ((location1.checked) || (location2.checked) || (location3.checked) || (location4.checked) || (location5.checked) || (location6.checked)) {
     location_m.textContent = "";
   }
   else {
     location_m.textContent = "Vous devez choisir une option";
-    e.preventDefault();
+    isValidForm = false;
   }
 
   // CONDITIONS A COCHER
   const checkbox1 = document.getElementById("checkbox1");
   const condition_m = document.getElementById("condition_erreur");
 
-  if(checkbox1.checked) {
+  if (checkbox1.checked) {
     condition_m = "";
   }
   else {
-    condition_m.textContent ="Vous devez vérifier que vous acceptez les termes et conditions";
-    e.preventDefault();
+    condition_m.textContent = "Vous devez vérifier que vous acceptez les termes et conditions";
+    isValidForm = false;
   }
-})
+
+  return isValidForm
+
+}
